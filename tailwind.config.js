f41f824181cb99e0c1e6fa3629b2b30cfb8b3ae7
Даніель Білодід <danielbilodid@MacBook-Air-Daniel.local>
2024-06-14 15:33:52 +0300

    finishing

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ECF2FF",
+        secondary: "#FFFFFF",
+        result: "#CAC9FF",
+        dark: "#303B59",
+        pink: "#fff7f6",
+        red: "#F55",
+        yellow: "#FFB21E",
+        lightYellow: "#fffbf4",
+        green: "#00BB8F",
+        lightGreen: "#f2fcf9",
+        blue: "#1125D6",
+        lightBlue: "#f3f4fd",
+      },
+      backgroundImage: {
+        "gradient-1":
+          "linear-gradient(180deg, #75F 0%, #6943FF 0.01%, #2F2CE9 100%)",
+        "gradient-2":
+          "linear-gradient(180deg, #4D21C9 0%, rgba(37, 33, 201, 0.00) 100%, rgba(37, 33, 201, 0.00) 100%)",
+      },
+    },
+  },
+
+  plugins: [],
+};
